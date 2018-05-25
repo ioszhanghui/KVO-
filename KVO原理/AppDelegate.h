e6f22d3964e30d5cf16ea96dba362bbcb89333a8
zhanghui <@ioszhanghui@163.com>
2018-05-25 09:58:05 +0800

    first commit

---
+++ KVO原理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO原理
+//
+//  Created by zhph on 2018/5/25.
+//  Copyright © 2018年 正和普惠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO原理/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVO原理
+//
+//  Created by zhph on 2018/5/25.
+//  Copyright © 2018年 正和普惠. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
